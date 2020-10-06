c001176ecd68989e106adb9f43db778638967be7
Klipach <klipach@mail.ru>
2020-10-06 16:36:36 +0300

    2 commit MongoDB

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ var guideRouter = require('./routes/guide');
 
 var app = express();
 
+app.use(express.json({ limit: '50mb' }));
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
+// Add headers
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/guide.js
+++ routes/guide.js
@@ -2,12 +2,64 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 const connectionString = 'mongodb://localhost:27017';
+const dbName = 'schooldb';
 
+
+
+async function asyncSelectCollection(collection_name) {
+    const client = await new MongoClient.connect(connectionString);
+    try {
+        const db = client.db(dbName);
+        const result = await db.collection(collection_name).find({}).toArray();
+        return JSON.stringify(result);
+    } finally {
+        client.close();
+    }
+}
+
+
+
+async function asyncCheckCollection(collection_name) {
+    const client = await new MongoClient.connect(connectionString);
+    try {
+        let bCheck = false;
+        const db = client.db(dbName);
+        const arrayRes = await db.listCollections().toArray();
+
+        for (index = 0; index < arrayRes.length; ++index) {
+            console.log(arrayRes[index].name);
+            if (arrayRes[index].name.toString() === collection_name.toString()) {
+                console.log(arrayRes[index].name.toString(), collection_name.toString());
+                bCheck = true;
+                break;
+            }
+        }
+
+        console.log('bCheck=', bCheck);
+        return bCheck;
+
+    } finally {
+        client.close();
+    }
+
+}
+
+async function asyncInsertLessonsName(collectionName, objLessonsName) {
+    const client = await new MongoClient.connect(connectionString);
+    try {
+        const db = client.db(dbName);
+        const rsIns = await db.collection(collectionName).insertMany(objLessonsName);
+        const result = await db.collection(collectionName).find({}).toArray();
+        return JSON.stringify(result);
+    } finally {
+        client.close();
+    }
+}
 
 async function insertWithSelectGuide() {
     const client = await new MongoClient.connect(connectionString);
     try {
-        const db = client.db("schooldb");
+        const db = client.db(dbName);
 
         const stuff2 = await db.collection('Stuff5').insertMany([ {d : 'три1'}, {d : 'три2'}, {d : 'три3'} ]);
 
@@ -22,9 +74,31 @@ async function insertWithSelectGuide() {
 
 router.get('/', async function(req, res, next) {
 
-    const guideList = await
-        insertWithSelectGuide();
-    res.send(guideList);
+    if (req.query.get_collection_check) {
+        const result = await asyncCheckCollection(req.query.collection_name);
+        res.send(result);
+    }
+
+    if (req.query.get_collection_test) {
+        const guideList = await insertWithSelectGuide();
+        res.send(guideList);
+    }
+
+    if (req.query.get_collection) {
+        const guideList = await asyncSelectCollection(req.query.collection_name);
+        res.send(guideList);
+    }
+
 
 });
+
+
+router.post('/', async function(req, res) {
+
+    if (req.body.insert_lessonsName) {
+        const result = await  asyncInsertLessonsName(req.body.collectionName, req.body.objLessonsName);
+        res.send(result);
+    }
+});
+
 module.exports = router;
