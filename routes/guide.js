5b893ff09ce79297fcaef35d686482a071425f3b
Klipach <klipach@mail.ru>
2020-11-05 14:16:08 +0300

    new server61

--- routes/guide.js
+++ routes/guide.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 const connectionString = 'mongodb://localhost:27017';
 const dbName = 'schooldb';
 
@@ -61,6 +62,25 @@ async function asyncInsertSummaryLesson(id_user, objSummaryLesson) {
         const db = client.db(dbName);
         const rsIns = await db.collection('listlessons').insertOne({id_user, objSummaryLesson});
         // const result = await db.collection('listlessons').find({id_user: id_user}).toArray();
+        return JSON.stringify(rsIns);
+    } finally {
+        client.close();
+    }
+}
+
+async function asyncUpdateSummaryLesson(id_key, objSummaryLesson2) {
+    const client = await new MongoClient.connect(connectionString);
+    id_key = id_key.replace(/"/g, '');
+
+    console.log('objSummaryLesson2=', objSummaryLesson2);
+
+    try {
+        const db = client.db(dbName);
+        const rsIns = await db.collection('listlessons').updateOne(
+            { _id : ObjectId(id_key)},
+            { $set:  {objSummaryLesson2}},
+            {upsert: true}
+        );
         return JSON.stringify(rsIns);
     } finally {
         client.close();
@@ -220,6 +240,12 @@ router.post('/', async function(req, res) {
         const result = await  asyncInsertSummaryLesson(req.body.id_user, req.body.objSummaryLesson);
         res.send(result);
     }
+
+    if (req.body.update_summarylesson) {
+        const result = await  asyncUpdateSummaryLesson(req.body.id_key, req.body.objSummaryLesson2);
+        res.send(result);
+    }
+
 
 });
 
