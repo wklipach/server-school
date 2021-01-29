5d2e34cb6d3a65bc2dd101e66ab2b35d2ee6ce04
wklipach <klipach@mail.ru>
2021-01-29 21:07:02 +0300

    1111111

--- SMTPServer.js
+++ SMTPServer.js
@@ -1,10 +1,23 @@
 var connectionSMTP = {
-    user:	"k*******@mail.ru",
-    password:"**********************",
+    user:	"klipach@mail.ru",
+    password:"bombardia22",
     host:	"smtp.mail.ru",
     port: "465",
     ssl:		true,
-    userForFromLetter: "k*****@mail.ru",
+    userForFromLetter: "klipach@mail.ru",
     subject: "Письмо с паролем"
 };
 module.exports=connectionSMTP;
+
+/*
+var connectionSMTP = {
+    user: "konstryktor-yroka-gbou25@mail.ru",
+    password:"verity2021",
+    host: "smtp.mail.ru",
+    port: "465",
+    ssl:                     true,
+    userForFromLetter: "konstryktor-yroka-gbou25@mail.ru",
+    subject: "Ваш новый пароль"
+};
+module.exports=connectionSMTP;
+*/
