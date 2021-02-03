4e89942eaee17510c98228c5e76a43fdbf647e6a
yalayas <yal@yal.spb.ru>
2021-02-03 15:33:43 +0300

    build 1

--- SMTPServer.js
+++ SMTPServer.js
@@ -1,23 +1,10 @@
 var connectionSMTP = {
-    user:	"klipach@mail.ru",
-    password:"bombardia22",
-    host:	"smtp.mail.ru",
-    port: "465",
-    ssl:		true,
-    userForFromLetter: "klipach@mail.ru",
-    subject: "Письмо с паролем"
-};
-module.exports=connectionSMTP;
-
-/*
-var connectionSMTP = {
-    user: "konstryktor-yroka-gbou25@mail.ru",
-    password:"verity2021",
+    user: "***@mail.ru",
+    password:"NEdeslp6",
     host: "smtp.mail.ru",
     port: "465",
     ssl:                     true,
-    userForFromLetter: "konstryktor-yroka-gbou25@mail.ru",
+    userForFromLetter: "***@mail.ru",
     subject: "Ваш новый пароль"
 };
 module.exports=connectionSMTP;
-*/
